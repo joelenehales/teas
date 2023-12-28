00cb11fc598082516988f5233e686cc60359c926
joelenehales <joelenehales@bell.net>
2023-12-28 18:27:05 -0500

    Upload setup/__init__ files

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+import codecs
+
+KEYWORDS = ["python","astronomy","jwst","spectroscopy"]
+INSTALL_REQUIRES = ["numpy","scipy","matplotlib","pandas","copy","astropy","jdaviz","reproject","photutils","specutils"]
+
+CURRENT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+
+def read(*parts):
+    with codecs.open(os.path.join(CURRENT_DIRECTORY, *parts), "rb", "utf-8") as f:
+        return f.read()
+
+if __name__ == "__main__":
+    setup(
+        name="teas", 
+        version="0.0.1",
+        author="Joelene Hales",
+        author_email="<joelenehales@bell.net>",
+        url="https://github.com/joelenehales/teas",
+        description='Tools for the Extraction and Analysis of Spectra from JWST observations',
+        long_description=read("README.rst"),
+        packages=find_packages(),
+        install_requires=INSTALL_REQUIRES,
+        keywords=KEYWORDS,
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Science/Research",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+    )
---
+++ teas/__init__.py
@@ -0,0 +1,3 @@
+from .helpers import helpers
+from .data_products import data_products
+from .spectra import spectra
