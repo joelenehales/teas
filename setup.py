473dbc699bf0fb9d714d28fa6cc910ddbeef1e18
joelenehales <joelenehales@bell.net>
2024-01-18 10:56:49 -0500

    Fix version number for re-upload to PyPI

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(*parts):
 if __name__ == "__main__":
     setup(
         name="teas", 
-        version="0.0.1",
+        version="0.0.2",
         author="Joelene Hales",
         author_email="<joelenehales@bell.net>",
         url="https://github.com/joelenehales/teas",
